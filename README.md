cf1ef1ec1ddb3165eeeb88e82c14fe697d1da4b3
stoyosawa <s.richspring@gmail.com>
2025-03-01 13:09:30 +1300

    sentiment updated (removed multilingual messages)

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 このリポジトリでは、[Streamlit](https://streamlit.io/)＋[Python](https://docs.python.org/ja/3/)ベースのWebアプリケーション2点のコードを公開しています。カードゲームのブラックジャックと、[Hugging Face](https://huggingface.co/)にあるモデルを用いた感情分析アプリです。これらはStreamlitコミュニティで運用されているので、以下のURLから実際に試すことができます。
 
-- Blackjack ... [https://sat-blackjack.streamlit.app/](https://sat-blackjack.streamlit.app/).
-- Sentiment ... [https://sat-sentiment.streamlit.app/](https://sat-sentiment.streamlit.app/).
+- Blackjack ... [https://sat-blackjack.streamlit.app/](https://sat-blackjack.streamlit.app/)
+- Sentiment analysis ... [https://sat-sentiment.streamlit.app/](https://sat-sentiment.streamlit.app/)
 
-Streamlitコミュニティ上のアプリケーションはしばらく使われていないと退蔵されます。そうした状態でアクセスがあると、まず「起こしますか？」（再度起動させる）というメッセージが出ます。クリックすれば展開が開始されますが、けっこう時間がかかります。とくに感情分析アプリは大きめなモデルやTransformersなどのパッケージがロードされる時間もあるので、かなり長く待たされます。
+Streamlitコミュニティ上のアプリケーションはしばらく使われていないと退蔵されます。そうした状態でアクセスがあると、まず「起こしますか？」（再度起動させる）というメッセージが出ます。クリックすれば展開が開始されますが、けっこう時間がかかります。とくに感情分析アプリは大きめなモデルやTransformersなどのパッケージがロードされる時間もあるので、かなり長く待たされます。ご了承のほどを。
 
 これらアプリケーションは、『[作ってわかる［入門］Streamlit](https://gihyo.jp/book/2025/978-4-297-14764-8)』（技術評論社，2025年2月刊，400頁）の第9章と第3章でそれぞれ紹介したものです。書籍のものとコードがやや異なりますが、エッセンスは同じです。
 
-The sentiment application uses the model trained on Japanese corpus. The program accepts any texts but you should not expect it outputs any meaningful result. 
+The sentiment application uses a model trained on Japanese corpus. The program accepts any texts but you should not expect it outputs any meaningful result. 
 
 【
 <a href="https://gihyo.jp/book/2025/978-4-297-14764-8">技術評論社</a> |
--- Sentiment/sentiment_analysis.py
+++ Sentiment/sentiment_analysis.py
@@ -3,32 +3,21 @@
 
 import streamlit as st
 from transformers import pipeline
-from acceptlang import parse_accept_lang_header
 
 
 MODEL = 'Mizuiro-sakura/luke-japanese-large-sentiment-analysis-wrime'
 MODEL_URL = 'https://huggingface.co/Mizuiro-sakura/luke-japanese-large-sentiment-analysis-wrime'
 MAX_LENGTH = 200
 
-
-# The order of sentiments list matters!
-MESSAGES = {
-    'en': {
-        'title': 'Sentiment Analysis (Transformers)',
-        'usage': f'Estimate the sentiment expressed in the sentence. The eight classes of sentiments (emotions) are: *joy, sadness, anticipation, surprise, anger, fear, disdust, and trust*. The most prevalent sentiment is selected. The Transformers model used is [`{MODEL}`](`{MODEL_URL}`).',
-        'sentiments': 'joy, sadness, anticipation, surprise, anger, fear, disdust, trust'.split(', '),
-        'prompt': 'Enter a Japanese sentence'
-    },
-    'ja': {
-        'title': 'AI 感情分析（Transformers 使用）',
-        'usage': f'入力した日本語文に書かれた感情を判定します。感情の種類は*喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼*の8種類で、文中で最も支配的なものが表示されます。使用しているモデルは[`{MODEL}`]({MODEL_URL})です。',
-        'sentiments': '喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼'.split('、'),
-        'prompt': '日本語の文を入力してください'
-    }
-    
+messages = {
+    'title': 'AI 感情分析（Transformers 使用）',
+    'usage': f'入力した日本語文に書かれた感情を判定します。感情の種類は*喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼*の8種類で、文中で最も支配的なものが表示されます。使用しているモデルは[`{MODEL}`]({MODEL_URL})です。',
+    'sentiments': '喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼'.split('、'),
+    'prompt': '日本語の文を入力してください'
 }
 
 
+@st.cache_resource
 def get_model(model=MODEL):
     try:
         pipe = pipeline('sentiment-analysis', model=model)
@@ -37,25 +26,12 @@ def get_model(model=MODEL):
     return pipe
 
 
-def find_language():
-    lang_default = 'en'
-    lang_list = list(MESSAGES.keys())
-    try:
-        al_value = st.context.headers['accept-language']
-        al_parsed = parse_accept_lang_header(al_value)
-        for lang in al_parsed:
-            if lang.name in lang_list:
-                return lang.name
-
-        return lang_default
-    except:
-        return lang_default
+with st.sidebar:
+    st.markdown('本アプリケーションは、[『作ってわかる［入門］Streamlit』](https://gihyo.jp/book/2025/978-4-297-14764-8)の第3章で紹介したものです。コードの説明やクラウドへの展開方法などはそちらを参照してください。')
+    st.image('http://image.gihyo.co.jp/assets/images/cover/2025/9784297147648.jpg')
 
 
 pipe = get_model()
-messages = MESSAGES[find_language()]
-
-
 st.title(messages['title'])
 st.markdown(messages['usage'])
 

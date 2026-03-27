e23144d91cb1fc178dc218dbf56539aebc1fde9c
stoyosawa <s.richspring@gmail.com>
2026-03-27 14:48:45 +1300

    sentiment analysis: Transformers err fixed.

--- Sentiment/sentiment_analysis.py
+++ Sentiment/sentiment_analysis.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env -S python -m streamlit run
 # 2024: ST Version 1.02
+# 2026: ST Version 1.10 ... Tokenizer 絡みの問題
 
 import streamlit as st
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
-
-MODEL = 'Mizuiro-sakura/luke-japanese-large-sentiment-analysis-wrime'
+MODEL_NAME = 'Mizuiro-sakura/luke-japanese-large-sentiment-analysis-wrime'
 MODEL_URL = 'https://huggingface.co/Mizuiro-sakura/luke-japanese-large-sentiment-analysis-wrime'
 MAX_LENGTH = 200
 
-messages = {
-    'title': 'AI 感情分析（Transformers 使用）',
-    'usage': f'入力した日本語文に書かれた感情を判定します。感情の種類は*喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼*の8種類で、文中で最も支配的なものが表示されます。使用しているモデルは[`{MODEL}`]({MODEL_URL})です。',
-    'sentiments': '喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼'.split('、'),
-    'prompt': '日本語の文を入力してください'
+MESSAGES = {
+    'jp': {
+        'title': 'AI 感情分析（Transformers 使用）',
+        'usage': f'入力した日本語文に書かれた感情を判定します。感情の種類は*喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼*の8種類で、文中で最も支配的なものが表示されます。使用しているモデルは[`{MODEL_NAME}`]({MODEL_URL})です。',
+        'sentiments': '喜び、悲しみ、期待、驚き、怒り、恐れ、嫌悪、信頼'.split('、'),
+        'prompt': '日本語の文を入力してください'
+    },
+    'en': {
+        'title': 'AI-Powered Sentiment Analysis (with Transformers)',
+        'usage': f'This app determines the emotion expressed in the Japanese text you enter. It classifies emotions into eight categories — joy, sadness, anticipation, surprise, anger, fear, disgust, and trust — and displays the one that is most dominant in the sentence. The model used for the analysis is [`{MODEL}`]({MODEL_URL}).',
+        'sentiments': 'joy, sadness, anticipation, surprise, anger, fear, disgust, trust'.split(', '),
+        'prompt': 'Enter a Japanese text'
+    }
 }
+LANG = 'jp'
 
+messages = MESSAGES[LANG]
 
 @st.cache_resource
-def get_model(model=MODEL):
+def get_model(model_name=MODEL_NAME):
     try:
-        pipe = pipeline('sentiment-analysis', model=model)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        pipe = pipeline("sentiment-analysis",
+            model=model,
+            tokenizer=tokenizer
+        )
     except Exception as e:
         st.markdown(e)
     return pipe
